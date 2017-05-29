8d8d1bf2c19537ee955b49b8e6cbbe6a20e37f83
lponce1405 <lponce1405@gmail.com>
2017-05-28 21:17:23 -0400

    Graficos Instalaciones

--- system/config3.php
+++ system/config3.php
@@ -4,6 +4,6 @@ $host_name = 'netlandchile.com';
 $user_name = 'wispland_netland';
 $pass_word = 'Netland16';
 $database_name = 'wispland_netland';
-$conn = mysql_connect($host_name, $user_name, $pass_word) or die ('Error connecting to mysql');
+$conn = mysql_pconnect($host_name, $user_name, $pass_word) or die ('Error connecting to mysql');
 mysql_select_db($database_name);
 ?>
