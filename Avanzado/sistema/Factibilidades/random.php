88e0d8282d46a7c5609e96274491a694caf66d49
lponce1405 <lponce1405@gmail.com>
2017-07-28 23:46:53 -0400

    Solucion

--- Avanzado/sistema/Factibilidades/random.php
+++ Avanzado/sistema/Factibilidades/random.php
@@ -1,16 +1,17 @@
 <?php
 include("../../../system/config.php");
 include("../../../services/config.php");
-$Sql = mysql_query("SELECT numero FROM TICKET ");
+$Sql = mysql_query("SELECT numero,id FROM TICKET ");
 $Ticket = 0;
 while($Record = mysql_fetch_array($Sql)){
     $Random = rand(100000,999999);
     echo $Ticket  = $Record[0];
+    $Id = $Record[1];
     if($Ticket==$Random){
         echo "Igual";
     }
     else{
-        mysql_query("UPDATE TICKET SET numero = $Random WHERE origen='WEB'");
+        mysql_query("UPDATE TICKET SET numero = $Random WHERE origen='WEB' AND id = $Id");
         echo "Actualizado";
     }
 }
